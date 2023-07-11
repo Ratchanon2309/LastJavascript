f46ef84e5e9a5e1517bea3a663c186dba7ed4fb6
Ratchanon2309 <euroeat25@gmail.com>
2023-07-11 18:41:29 +0700

    create ex03.js

---
+++ 2/ex03.js
@@ -0,0 +1,11 @@
+function numbersplit(a) {
+    let num1 = [Math.floor(a / 2), Math.ceil(a / 2)];
+    return num1;
+
+    
+}
+console.log(numbersplit(4))
+console.log(numbersplit(10))
+console.log(numbersplit(11))
+console.log(numbersplit(-9))
+
