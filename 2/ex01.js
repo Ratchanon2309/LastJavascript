38f8a83ca7fb9a315c3d35e9e7d415714274023c
Ratchanon2309 <euroeat25@gmail.com>
2023-07-11 17:23:57 +0700

    update

--- 2/ex01.js
+++ 2/ex01.js
@@ -1,10 +1,10 @@
-function parameter(a,b) {
+function lessthan100(a,b) {
     if (a + b <= 100) {
         return true;
     } else {
         return false
     }
 }
-console.log(parameter(22,15))
-console.log(parameter(83,34))
-console.log(parameter(3,77))
+console.log(lessthan100(22,15))
+console.log(lessthan100(83,34))
+console.log(lessthan100(3,77))
