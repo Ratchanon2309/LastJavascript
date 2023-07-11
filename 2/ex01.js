cf4cbade5dd79037c43a39a169b90d6ccc622ff1
Ratchanon2309 <euroeat25@gmail.com>
2023-07-11 17:17:39 +0700

    create ex01.js

---
+++ 2/ex01.js
@@ -0,0 +1,10 @@
+function parameter(a,b) {
+    if (a + b <= 100) {
+        return true;
+    } else {
+        return false
+    }
+}
+console.log(parameter(22,15))
+console.log(parameter(83,34))
+console.log(parameter(3,77))
