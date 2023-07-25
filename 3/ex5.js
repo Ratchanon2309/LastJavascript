14f5d755deaf5a26f23501cc82f0447ae1e6803b
Ratchanon2309 <euroeat25@gmail.com>
2023-07-25 12:28:08 +0700

    create

--- 3/ex5.js
+++ 3/ex5.js
@@ -1,5 +1,19 @@
 function countPosSumNeg(a) {
-    
+    let positiveNum = 0;
+    let negativeNum = 0;
+
+    for (let index = 0; index < a.length; index++) {
+        if (a[index] > 0) {
+            positiveNum++;
+        } else if (a[index] < 0) {
+            negativeNum += a[index];
+        }
+        
+    }
+    return a.length === 0?[]:
+    [positiveNum,negativeNum];
 }
-console.log(countPosSumNeg);
-console.log
+console.log(countPosSumNeg([1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15]));
+console.log(countPosSumNeg([92,6,73,-77,81,-90,99,8,-85,34]));
+console.log(countPosSumNeg([91,-4,80,-73,-28]));
+console.log(countPosSumNeg([]));
